e8957ace2a8c100361b24f9baa0ce7c5674386a7
Tommaso Pajero <tommaso.pajero@cern.ch>
2022-09-28 12:53:34 +0200

    merge developments branch of gammacombo

--- core/include/rdtsc.h
+++ core/include/rdtsc.h
@@ -21,32 +21,20 @@ static __inline__ unsigned long long rdtsc(void)
     unsigned long long int result=0;
     unsigned long int upper, lower,tmp;
     __asm__ volatile(
-	    "0:                  \n"
-	    "\tmftbu   %0           \n"
-	    "\tmftb    %1           \n"
-	    "\tmftbu   %2           \n"
-	    "\tcmpw    %2,%0        \n"
-	    "\tbne     0b         \n"
-	    : "=r"(upper),"=r"(lower),"=r"(tmp)
-	    );
+        "0:                  \n"
+        "\tmftbu   %0           \n"
+        "\tmftb    %1           \n"
+        "\tmftbu   %2           \n"
+        "\tcmpw    %2,%0        \n"
+        "\tbne     0b         \n"
+        : "=r"(upper),"=r"(lower),"=r"(tmp)
+        );
     result = upper;
     result = result<<32;
     result = result|lower;
 
     return(result);
 }
-#elif defined(__aarch64__)
-static __inline__ unsigned long long rdtsc(void)
-{
-    // https://github.com/google/benchmark/blob/v1.1.0/src/cycleclock.h#L116
-
-    // System timer of ARMv8 runs at a different frequency than the CPU's.
-    // The frequency is fixed, typically in the range 1-50MHz.  It can be
-    // read at CNTFRQ special register.  We assume the OS has set up
-    // the virtual timer properly.
-    int64_t virtual_timer_value;
-    asm volatile("mrs %0, cntvct_el0" : "=r"(virtual_timer_value));
-    return virtual_timer_value;
 #endif
 
 #endif // _RDTSC_H
--- setup-env-cvmfs.sh
+++ setup-env-cvmfs.sh
@@ -1,1 +1,4 @@
-. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_101 x86_64-centos7-gcc10-opt
+#!/bin/bash
+export LCG_VERSION=LCG_102 
+export BINARY_TAG=x86_64-centos7-gcc8-opt 
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh $LCG_VERSION $BINARY_TAG
