03d0f63580551d35c11add9a5a7b8534eb386a03
Tommaso Pajero <72619898+tpajero@users.noreply.github.com>
2026-08-20 14:31:42 +0200

    refactor code (#21)

--- BLUE/include/BLUE/Utils.h
+++ BLUE/include/BLUE/Utils.h
@@ -9,14 +9,35 @@
 
 namespace BLUE {
 
-  /// Named combination: human-readable title plus the indices of active estimates.
-  using Combination = std::pair<std::string, std::vector<int>>;
+  /**
+   * Representation of a measurement, identified by a name and containing a vector containing:
+   * - the estimate of the observable;
+   * - its statistical uncertainty;
+   * - the systematic uncertainties (uncertainties that are correlated between different estimates must be listed
+   *   separately).
+   */
+  struct Estimate {
+    std::string name;
+    std::vector<double> vals;
+  };
+
+  using Estimates = std::vector<Estimate>;
+
+  /// Named combination: human-readable title plus the names of active estimates.
+  using Combination = std::pair<std::string, std::vector<std::string>>;
 
   /// Map combining integer flags to named combinations.
   using Combinations = std::map<int, Combination>;
 
-  /// Print a banner with the ID of the current executable, plus the title for the combination being run.
-  void print_banner(const std::string& combiner_title, const std::string& avg_title);
+  struct OutputFormat {
+    TString for_val = "%+4.2f";
+    TString for_unc = "%2.2f";
+    TString for_wei = "%2.2f";
+    TString for_rho = "%2.2f";
+    TString for_pul = "%2.2f";
+    TString for_chi = "%2.2f";
+    TString for_uni = "";
+  };
 
   /**
    * Parse the arguments of the main function of BLUE executables.
@@ -25,6 +46,25 @@ namespace BLUE {
    * which instead prints all available combinations, including flag, title, and the names of the measurements that
    * it includes.
    */
-  int parse_args(int argc, char** argv, const Combinations& combinations, const std::vector<TString>& names);
+  int parse_args(int argc, char** argv, const Combinations& combinations);
+
+  /**
+   * Run the combination of a set of 1D estimates.
+   *
+   * @param flag The integer flag identifying the combination to be run.
+   * @param combo_category The human-readable title of the category of combinations run by the executable, to be
+   * printed in the banner.
+   * @param combinations The map of all available combinations.
+   * @param estimates The vector of all estimates that the combinations are based on.
+   * @param names_obs The vector containing the name of the observable.
+   * @param names_unc The vector of names of the uncertainties, in the order they appear in the `vals` vector of each
+   * estimate.
+   * @param rho The vector of correlation coefficients for the uncertainties, in the order they appear in `names_unc`.
+   * @param format The output format for printing the results.
+   */
+  void run_combination(const int flag, const std::string& combo_category, const Combinations& combinations,
+                       const Estimates& estimates, const std::vector<TString>& names_obs,
+                       const std::vector<TString>& names_unc, const std::vector<double>& rho,
+                       const OutputFormat& format);
 
 }  // namespace BLUE
--- BLUE/main/ycp.cpp
+++ BLUE/main/ycp.cpp
@@ -11,93 +11,34 @@
  *
  */
 
-#include <BLUE/Blue.h>
 #include <BLUE/Utils.h>
 
-#include <TMatrixD.h>
 #include <TString.h>
 
-#include <cstdlib>
-#include <memory>
-#include <stdexcept>
 #include <vector>
 
-const BLUE::Combinations combinations = {
-    {0, {"World average 2015", {0, 1}}},
+const BLUE::Estimates estimates = {
+    // clang-format off
+    //                Val    Stat    Syst
+    {"Belle 2009",  {  1.1 ,  6.1 ,  5.2}},  // https://inspirehep.net/literature/821323   D0 -> KS0 K+ K-
+    {"BESIII 2015", {-20.0 , 13.0 ,  7.0}},  // https://inspirehep.net/literature/1337473
+    // clang-format on
 };
+const std::vector<double> rho = {0., 0.};
+const std::vector<TString> names_obs = {"    yCP"};
+const std::vector<TString> names_unc = {"  Stat", "  Syst"};
 
-const std::vector<TString> names = {
-    "Belle 2009 ",  //  0  https://inspirehep.net/literature/821323
-    "BESIII 2015",  //  1  https://inspirehep.net/literature/1337473
+const BLUE::Combinations combinations = {
+    {0, {"World average 2015", {"Belle 2009", "BESIII 2015"}}},
 };
 
 /**
- * Perform the combination of a given set of measurements.
- *
- * @param flag Steers which set of measurements should be employed (according to the values of "combinations" above).
+ * Perform the combination of a set of yCP (CP-even) measurements.
  */
-void run(const int flag) {
-
-  const std::vector<TString> names_obs = {"    yCP"};
-  const auto num_est = names.size();
-
-  // Array of estimates and uncertainties
-  const std::vector<TString> names_unc = {"  Stat", "  Syst"};
-
-  const std::vector<double> x_est = {
-      // clang-format off
-      // Val  Stat    Syst
-        1.1 ,  6.1 ,  5.2,  // Belle 2009, D0 -> KS0 K+ K-
-      -20.0 , 13.0 ,  7.0,  // BESIII 2015
-      // clang-format on
-  };
-  const auto num_unc = names_unc.size();
-  if (x_est.size() != num_est * (num_unc + 1))
-    throw std::runtime_error("The size of the uncertainty matrix is inconsistent with the number of estimates and the "
-                             "number of uncertainties per estimate");
-  auto inp_est = std::make_unique<const TMatrixD>(num_est, num_unc + 1, &x_est[0]);
-
-  // Statistical and systematic uncertainties are not correlated among the two measurements
-  const std::vector<double> rho_val = {0., 0.};
-  if (rho_val.size() != num_unc)
-    throw std::runtime_error("The size of the correlation vector is inconsistent with the number of uncertainties");
-
-  // Statistical precision in systematic uncertainties
-  const std::vector<double> s_unc(num_est * num_unc, 0.);
-  auto inp_sta = std::make_unique<const TMatrixD>(num_est, num_unc, &s_unc[0]);
-
-  // Format for the output
-  const TString for_val = "%+6.2f";
-  const TString for_unc = "%5.2f";
-  const TString for_wei = "%2.2f";
-  const TString for_rho = "%2.2f";
-  const TString for_pul = "%2.2f";
-  const TString for_chi = "%2.2f";
-  const TString for_uni = "";
-
-  // Initialise the combiner
-  auto my_blue = std::make_unique<Blue>(num_est, num_unc);
-  my_blue->SetFormat(for_val, for_unc, for_wei, for_rho, for_pul, for_chi, for_uni);
-  my_blue->FillNamEst(&names[0]);
-  my_blue->FillNamUnc(&names_unc[0]);
-  my_blue->FillNamObs(&names_obs[0]);
-  my_blue->FillEst(inp_est.get());
-  my_blue->FillSta(inp_sta.get());
-  for (int i = 0; i < num_unc; ++i) my_blue->FillCor(i, rho_val[i]);
-
-  // Perform the combination
-  const auto [title, inputs] = combinations.at(flag);
-  BLUE::print_banner("yCP (CP-even)", title);
-  for (auto i = 0; i < num_est; ++i) my_blue->SetInActiveEst(i);
-  for (auto i : inputs) my_blue->SetActiveEst(i);
-  my_blue->FixInp();
-  my_blue->PrintEst();
-  my_blue->Solve();
-  my_blue->PrintResult();
-}
-
 int main(int argc, char** argv) {
-  const int flag = BLUE::parse_args(argc, argv, combinations, names);
-  run(flag);
+  const int flag = BLUE::parse_args(argc, argv, combinations);
+
+  BLUE::run_combination(flag, "yCP (CP-even)", combinations, estimates, names_obs, names_unc, rho,
+                        BLUE::OutputFormat{});
   return EXIT_SUCCESS;
 }
